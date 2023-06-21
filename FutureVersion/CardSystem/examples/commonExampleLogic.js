96c44a5cb61005fe665da0d0cc04161317778bda
Vincent-eyekandy <vincent@eyekandy.com>
2023-06-21 01:09:13 +0100

    adding jdoc and comments to the function

--- FutureVersion/CardSystem/examples/commonExampleLogic.js
+++ FutureVersion/CardSystem/examples/commonExampleLogic.js
@@ -4,13 +4,25 @@ const {
   ParseResponseToOrders,
 } = require("../../PlayerQuestions");
 
+/**
+ * This function takes an orderDeck as an argument
+ * and returns an orderFromResponse based on user input.
+ * @param {Array<Card>} orderDeck - The deck of orders from which to choose
+ * @returns {string} The order chosen by the player
+ */
 function GetOrderFromOrderDeck(orderDeck) {
+  // Get an array of possible orders from the orderDeck
   const possibleOrdersArray = getNamesOfCardsInDeck(orderDeck);
+
+  // Join the possible orders into a string with newlines
   const orderNamesWithNewline = possibleOrdersArray.join("\n");
+
+  // Ask the player to choose an order from the list
   const response = AskForPlayerResponse(
     `choose \n${orderNamesWithNewline} \n:`
   );
 
+  // Parse the player's response and return the corresponding order
   const orderFromResponse = ParseResponseToOrders(
     response,
     possibleOrdersArray
