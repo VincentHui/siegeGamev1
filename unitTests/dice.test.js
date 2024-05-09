7b16c233580dd1ca078eda6678e1fc3b7be3d42e
benballerboi <bhui@ryzen-2700x-pc>
2024-05-09 12:22:09 -0700

    I added a new unit test

--- unitTests/dice.test.js
+++ unitTests/dice.test.js
@@ -24,3 +24,16 @@ test("if 2 dice are rolled with 12 sides, each dice should roll more that", () =
   }
   expect(totalCalls).toBe(100);
 });
+
+test("if 3 8 sided dice are rolled, the total value of the dice should be less than 24", () => {
+  let totalCalls = 0;
+  for (let i = 0; i < 50; i++) {
+    const rollResult = diceRoll(3, 8);
+    rollResult.forEach((element) => {
+      expect(rollResult).toBeLessThan(24);
+      expect(element).toBeGreaterThan(0);
+    });
+    totalCalls = totalCalls + 1;
+  }
+  expect(totalCalls).toBe(50);
+});
