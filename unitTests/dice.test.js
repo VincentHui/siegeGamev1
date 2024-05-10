91d00c3edfa899bf3676e9909b15e0263a265f57
Samuel Hui <samwise47@gmail.com>
2024-05-10 12:59:36 -0700

    Small unit test for dice roll operations

--- unitTests/dice.test.js
+++ unitTests/dice.test.js
@@ -12,6 +12,18 @@ test("if 2 dice are rolled result array should always be 2", () => {
   expect(totalCalls).toBe(100);
 });
 
+test("if dice rolled have 15 sides, each dice should roll between 0 and 16", () => {
+  let totalCalls = 0;
+  for (let i = 0; i < 100; i++) {
+    rollResult.forEach((element) => {
+      expect(element).toBeLessThan(16);
+      expect(element).toBeGreaterThan(0);
+    });
+    totalCalls = totalCalls + 1;
+  }
+  expect(totalCalls).toBe(100);
+});
+
 test("if 2 dice are rolled with 12 sides, each dice should roll more that", () => {
   let totalCalls = 0;
   for (let i = 0; i < 100; i++) {
