6c11c011b401492041480e6ac1a71fe5a8a073e6
Samuel Hui <samwise47@gmail.com>
2024-06-13 12:35:33 -0700

    deleted trash

--- FutureVersion/CardSystem/turnlevel.js
+++ FutureVersion/CardSystem/turnlevel.js
@@ -3,39 +3,6 @@ const { Eventcards, Manpower, Morale } = require("./eventcards");
 // let Manpower = 2000;
 // let Morale = 10;
 
-// let Manpower = 2000;
-// let Morale = 10;
-
-// let Eventcards = [
-//   {
-//     name: "Thunderstorm",
-//     description:
-//       "A brewing storm opens the heavens. Rain pours down and soaks everyman to the bone.",
-//     effect() {
-//       console.log("I work");
-//       Manpower = Manpower - 500;
-//     },
-//   },
-
-//   {
-//     name: "Captured Spy",
-//     description:
-//       "Some of our men captured a suspicious man walking among the camp.",
-//     effect: function () {
-//       Morale + 1;
-//     },
-//   },
-//   {
-//     name: "Sign in the Heavens",
-//     description:
-//       "A blazing star fell from the heavens to the earth, our men wonder at the sign.",
-//     effect: function (Morale) {
-//       const SignsMorale = Math.random() > 0.4 ? 5 : -15;
-//       Morale += SignsMorale;
-//     },
-//   },
-// ];
-
 function Turnbase(turn, numberofturns, callback) {
   for (let i = turn; i < numberofturns; i++) {
     console.log([turn]);
