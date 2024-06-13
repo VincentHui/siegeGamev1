cea8894834e05c4f04294d90e0aad75f25742015
Samuel Hui <samwise47@gmail.com>
2024-06-13 11:36:24 -0700

    Turn system with callback function for events

---
+++ FutureVersion/CardSystem/eventcards.js
@@ -0,0 +1,32 @@
+let Manpower = 2000;
+let Morale = 10;
+
+let Eventcards = [
+  {
+    name: "Thunderstorm",
+    description:
+      "A brewing storm opens the heavens. Rain pours down and soaks everyman to the bone.",
+    effect: function () {
+      Manpower -= 500;
+    },
+  },
+
+  {
+    name: "Captured Spy",
+    description:
+      "Some of our men captured a suspicious man walking among the camp.",
+    effect: function () {
+      Morale += 1;
+    },
+  },
+  {
+    name: "Sign in the Heavens",
+    description:
+      "A blazing star fell from the heavens to the earth, our men wonder at the sign.",
+    effect: function (Morale) {
+      const SignsMorale = Math.random() > 0.4 ? 5 : -15;
+      Morale += SignsMorale;
+    },
+  },
+];
+module.exports = { Eventcards, Manpower, Morale };
--- FutureVersion/CardSystem/turnlevel.js
+++ FutureVersion/CardSystem/turnlevel.js
@@ -1,12 +1,29 @@
-let Manpower = 2000;
+const { Eventcards, Manpower, Morale } = require("./eventcards");
 
-function Turnbase(turn, numberofturns) {
+// let Manpower = 2000;
+// let Morale = 10;
+
+function Turnbase(turn, numberofturns, callback) {
   for (let i = turn; i < numberofturns; i++) {
     console.log([turn]);
-    Manpower = Manpower + 1000;
+    callback();
+    // Manpower = Manpower + 1000;
     console.log("You have", Manpower, "men");
+    console.log("Army morale at", Morale);
     turn++;
   }
 }
 
-Turnbase(2, 20);
+function getRandomEventCard() {
+  const randomIndex = Math.floor(Math.random() * Eventcards.length);
+  return Eventcards[randomIndex];
+}
+
+function playEventCard() {
+  const randomCard = getRandomEventCard();
+  console.log(`Playing: ${randomCard.name} - ${randomCard.description}`);
+  return randomCard.effect();
+}
+
+Turnbase(2, 20, playEventCard);
+// -------------------------------------------------------------------------
