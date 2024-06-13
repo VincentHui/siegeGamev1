2afebb7706c05a5ccf975b972ecd7148f9c28e59
Samuel Hui <samwise47@gmail.com>
2024-06-13 12:22:36 -0700

    Updated effect function in array

--- FutureVersion/CardSystem/eventcards.js
+++ FutureVersion/CardSystem/eventcards.js
@@ -6,8 +6,9 @@ let Eventcards = [
     name: "Thunderstorm",
     description:
       "A brewing storm opens the heavens. Rain pours down and soaks everyman to the bone.",
-    effect: function () {
-      Manpower - 500;
+    effect() {
+      console.log("I work");
+      Manpower = Manpower - 500;
     },
   },
 
@@ -15,17 +16,18 @@ let Eventcards = [
     name: "Captured Spy",
     description:
       "Some of our men captured a suspicious man walking among the camp.",
-    effect: function () {
-      Morale + 1;
+    effect() {
+      console.log("The men thirst for revenge");
+      Morale = Morale + 1;
     },
   },
   {
     name: "Sign in the Heavens",
     description:
       "A blazing star fell from the heavens to the earth, our men wonder at the sign.",
-    effect: function (Morale) {
+    effect() {
       const SignsMorale = Math.random() > 0.4 ? 5 : -15;
-      Morale += SignsMorale;
+      Morale = Morale += SignsMorale;
     },
   },
 ];
